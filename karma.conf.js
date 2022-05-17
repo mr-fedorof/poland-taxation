056910229ab665a020185987006d05f4fd3d2d4e
Aliaksandr Fedarau <mr.alex.fedorof@gmail.com>
2022-05-18 00:22:05 +0200

    rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/join-poland'),
+      dir: require('path').join(__dirname, './coverage/poland-taxation'),
       subdir: '.',
       reporters: [
         { type: 'html' },
